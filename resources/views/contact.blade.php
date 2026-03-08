1f213fbae0f7f685fdc1e38c958be203e8091efb
adlesrtp-dotcom <adlesrtp@gmail.com>
2026-03-08 14:37:13 +0700

    first commit

---
+++ resources/views/contact.blade.php
@@ -0,0 +1,6 @@
+<div>
+    <h1>Contact Kami</h1>
+    <p>Silakan menghubungi kami untuk informasi lebih lanjut.</p>
+    <p>Email: StayEase@gmail.com</p>
+    <p>Telepon: 0813-71170-3569</p>
+</div>
